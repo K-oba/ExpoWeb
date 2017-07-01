08d85614780de4863a4004a15a4c1a60eb17b880
Robert <robjamar@gmail.com>
2017-07-01 11:55:22 -0600

    Fix invite

--- expoCR/src/main/webapp/app/entities/exposicion/exposicion.state.js
+++ expoCR/src/main/webapp/app/entities/exposicion/exposicion.state.js
@@ -74,6 +74,34 @@
                 }]
             }
         })
+        .state('exposicion-detail.invite', {
+            parent: 'exposicion',
+            url: '/exposicion/{id}',
+            data: {
+                authorities: ['ROLE_USER'],
+                pageTitle: 'Exposicion'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/entities/exposicion/exposicion-invite-dialog.html',
+                    controller: 'ExposicionDetailController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                entity: ['$stateParams', 'Exposicion', function($stateParams, Exposicion) {
+                    return Exposicion.get({id : $stateParams.id}).$promise;
+                }],
+                previousState: ["$state", function ($state) {
+                    var currentStateData = {
+                        name: $state.current.name || 'exposicion',
+                        params: $state.params,
+                        url: $state.href($state.current.name, $state.params)
+                    };
+                    return currentStateData;
+                }]
+            }
+        })
         .state('exposicion-detail.edit', {
             parent: 'exposicion-detail',
             url: '/detail/edit',
@@ -141,31 +169,6 @@
             onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
                 $uibModal.open({
                     templateUrl: 'app/entities/exposicion/exposicion-dialog.html',
-                    controller: 'ExposicionDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['Exposicion', function(Exposicion) {
-                            return Exposicion.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('exposicion', null, { reload: 'exposicion' });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('exposicion.invite', {
-            parent: 'exposicion',
-            url: '/{id}/edit',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/exposicion/exposicion-invite-dialog.html',
                     controller: 'ExposicionDialogController',
                     controllerAs: 'vm',
                     backdrop: 'static',
