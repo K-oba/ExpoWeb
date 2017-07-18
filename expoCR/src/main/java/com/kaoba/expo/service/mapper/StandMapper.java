c40f7bd91dc55d4e196b4c9261e6c28abfda0c24
jimmi <jimmivco@hotmail.com>
2017-07-18 12:02:09 -0600

    tipo -> descripcion   stand

--- expoCR/src/main/java/com/kaoba/expo/service/mapper/StandMapper.java
+++ expoCR/src/main/java/com/kaoba/expo/service/mapper/StandMapper.java
@@ -16,16 +16,18 @@ public interface StandMapper extends EntityMapper <StandDTO, Stand> {
     @Mapping(source = "click.id", target = "clickId")
 
     @Mapping(source = "beacon.id", target = "beaconId")
-    StandDTO toDto(Stand stand); 
+    StandDTO toDto(Stand stand);
     @Mapping(target = "usuario", ignore = true)
 
     @Mapping(source = "brouchureId", target = "brouchure")
+
+    @Mapping(source = "tipo", target = "tipo")
 
     @Mapping(source = "clickId", target = "click")
 
     @Mapping(source = "beaconId", target = "beacon")
     @Mapping(target = "exposicions", ignore = true)
-    Stand toEntity(StandDTO standDTO); 
+    Stand toEntity(StandDTO standDTO);
     default Stand fromId(Long id) {
         if (id == null) {
             return null;
